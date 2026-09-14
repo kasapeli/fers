a07a90ee29706c2d0ba2f522e11967f3c3cd0f4b
kasapeli <yanratanaksambath@gmail.com>
2026-09-14 19:18:43 +0700

    idt.rs: add timer

--- src/arch/x86_64/idt.rs
+++ src/arch/x86_64/idt.rs
@@ -2,8 +2,9 @@ use lazy_static::lazy_static;
 use pic8259::ChainedPics;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
-use crate::arch::x86_64::gdt;
+use crate::{arch::x86_64::gdt, print};
 
+// pic
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 1;
 
@@ -19,8 +20,21 @@ lazy_static! {
                 .set_handler_fn(df_handler)
                 .set_stack_index(gdt::DF_IST)
         };
+        idt[InterruptIndex::Timer.as_u8()].set_handler_fn(timer_interrupt_handler);
         idt
     };
+}
+
+#[derive(Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+}
+
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
 }
 
 pub fn init() {
@@ -36,4 +50,12 @@ pub extern "x86-interrupt" fn df_handler(stack_frame: InterruptStackFrame, code:
         "EXCEPTION: DOUBLE FAULT\n{:#?}\nCode: {}",
         stack_frame, code
     );
+}
+
+pub extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
+    print!(".");
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
 }

f62408cc29e3b1e8ed6571ac89b6c8c87399a926
kasapeli <yanratanaksambath@gmail.com>
2026-09-14 18:17:00 +0700

    idt.rs: add double fault handler

--- src/arch/x86_64/idt.rs
+++ src/arch/x86_64/idt.rs
@@ -1,11 +1,17 @@
-use crate::print;
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+
+use crate::arch::x86_64::gdt;
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(df_handler)
+                .set_stack_index(gdt::DF_IST)
+        };
         idt
     };
 }
@@ -15,5 +21,12 @@ pub fn init() {
 }
 
 pub extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
-    print!("{:#?}", stack_frame);
+    panic!("{:#?}", stack_frame);
+}
+
+pub extern "x86-interrupt" fn df_handler(stack_frame: InterruptStackFrame, code: u64) -> ! {
+    panic!(
+        "EXCEPTION: DOUBLE FAULT\n{:#?}\nCode: {}",
+        stack_frame, code
+    );
 }

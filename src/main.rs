eff434601e9d41b48a6b229942a1cd09ec66c920
kasapeli <yanratanaksambath@gmail.com>
2026-09-14 13:37:06 +0700

    main.rs: add idt test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+#![feature(abi_x86_interrupt)]
 #![no_std]
 #![no_main]
 
+mod arch;
 mod drivers;
 mod flib;
 
+use crate::arch::x86_64::idt::init;
 use core::panic::PanicInfo;
+use x86_64::{self, instructions};
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -14,5 +18,7 @@ fn panic(_info: &PanicInfo) -> ! {
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     print!("something");
+    init();
+    instructions::interrupts::int3();
     loop {}
 }

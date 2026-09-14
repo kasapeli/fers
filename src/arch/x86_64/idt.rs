9ca4c636a78f8e73190c428f6da5bccba7f1610d
kasapeli <yanratanaksambath@gmail.com>
2026-09-14 13:36:26 +0700

    idt.rs: init

--- src/arch/x86_64/idt.rs
+++ src/arch/x86_64/idt.rs
@@ -1,1 +1,19 @@
-a
+use crate::print;
+use lazy_static::lazy_static;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
+
+pub fn init() {
+    IDT.load();
+}
+
+pub extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
+    print!("{:#?}", stack_frame);
+}

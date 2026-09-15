6a4894a9e698ff435d787509a183a9d47c800823
kasapeli <yanratanaksambath@gmail.com>
2026-09-16 01:26:17 +0700

    main.rs: upd with heap

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,16 @@
 mod arch;
 mod drivers;
 mod flib;
+mod memory;
 
 use crate::{
     arch::x86_64::{gdt, idt},
     flib::hlt,
 };
+
+use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
-use x86_64;
+use x86_64::{self, VirtAddr};
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -20,12 +23,20 @@ fn panic(info: &PanicInfo) -> ! {
     hlt::exec();
 }
 
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    println!("something");
+entry_point!(kernel);
+
+fn kernel(boot_info: &'static BootInfo) -> ! {
+    use memory::pager::BootInfoFrameAllocator;
 
     gdt::init();
     idt::init();
+
+    let phy_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::pager::init(phy_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    memory::ll_alloc::init_heap(&mut mapper, &mut frame_allocator).expect("h");
+
     unsafe { idt::PICS.lock().initialize() };
     x86_64::instructions::interrupts::enable();
 

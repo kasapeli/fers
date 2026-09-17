b99c5e1ee5a2d4e57fa61b4d6eb4494a960b2ba4
kasapeli <yanratanaksambath@gmail.com>
2026-09-17 15:45:03 +0700

    time.rs: add arg matching

--- src/utils/builtins/time.rs
+++ src/utils/builtins/time.rs
@@ -1,11 +1,16 @@
-use crate::{drivers::cmos::fetch_time, println, utils::builtins::presets::arg_err::exceed};
+use super::presets::arg_err::invalid;
+use crate::drivers::cmos::*;
+use crate::println;
 
 pub fn exec(content: &[&str]) {
-    if content.len() > 1 {
-        println!("{}", exceed(1));
+    if content.len() == 1 {
+        all();
+    } else {
+        match content[1] {
+            "-s" => second(),
+            "-m" => minute(),
+            "-h" => hour(),
+            _ => println!("{}", invalid()),
+        }
     }
-
-    let raw = content[0];
-
-    fetch_time(raw);
 }

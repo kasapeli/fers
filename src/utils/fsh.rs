b809ff50eb68e7ee0bb5bb7d44f45fbd702aa283
kasapeli <yanratanaksambath@gmail.com>
2026-09-17 23:29:39 +0700

    fsh.rs: uncomment poke

--- src/utils/fsh.rs
+++ src/utils/fsh.rs
@@ -78,9 +78,9 @@ fn parse(content: Vec<&str>) {
         "time" => {
             time::exec(&content);
         }
-        // "poke" => {
-        //     poke::handle(&content);
-        // }
+        "poke" => {
+            poke::handle(&content);
+        }
         "reboot" => {
             println!("Rebooting...");
             unsafe {

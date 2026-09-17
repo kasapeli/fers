1cd2a11f03562c53ac5ed77b6d3459de6307791d
kasapeli <yanratanaksambath@gmail.com>
2026-09-17 15:44:52 +0700

    cmos.rs: shift handling to time.rs

--- src/drivers/cmos.rs
+++ src/drivers/cmos.rs
@@ -4,35 +4,41 @@ use x86_64::instructions::port::Port;
 
 use crate::println;
 
-pub fn fetch_time(format: &str) {
-    // consider returning u8 instead later
-    match format {
-        "s" => unsafe {
-            Port::<u8>::new(0x70).write(0x00);
-            let second = Port::<u8>::new(0x71).read();
-            println!("{}", second);
-        },
-        "m" => unsafe {
-            Port::<u8>::new(0x70).write(0x02);
-            let minute = Port::<u8>::new(0x71).read();
-            println!("{}", minute);
-        },
-        "h" => unsafe {
-            Port::<u8>::new(0x70).write(0x04);
-            let hour = Port::<u8>::new(0x71).read();
-            println!("{}", hour);
-        },
-        _ => unsafe {
-            Port::<u8>::new(0x70).write(0x00); // this is sooo messy dude
-            let second = Port::<u8>::new(0x71).read();
+pub fn second() {
+    unsafe {
+        Port::<u8>::new(0x70).write(0x00);
+        let second = Port::<u8>::new(0x71).read();
+        println!("{}", second);
+    }
+}
 
-            Port::<u8>::new(0x70).write(0x02);
-            let minute = Port::<u8>::new(0x71).read();
+pub fn minute() {
+    unsafe {
+        Port::<u8>::new(0x70).write(0x02);
+        let minute = Port::<u8>::new(0x71).read();
+        println!("{}", minute);
+    }
+}
 
-            Port::<u8>::new(0x70).write(0x04);
-            let hour = Port::<u8>::new(0x71).read();
-            println!("{}:{}:{}", hour, minute, second);
-        },
+pub fn hour() {
+    unsafe {
+        Port::<u8>::new(0x70).write(0x04);
+        let hour = Port::<u8>::new(0x71).read();
+        println!("{}", hour);
+    }
+}
+
+pub fn all() {
+    unsafe {
+        Port::<u8>::new(0x70).write(0x00); // this is sooo messy dude
+        let second = Port::<u8>::new(0x71).read();
+
+        Port::<u8>::new(0x70).write(0x02);
+        let minute = Port::<u8>::new(0x71).read();
+
+        Port::<u8>::new(0x70).write(0x04);
+        let hour = Port::<u8>::new(0x71).read();
+        println!("{}:{}:{}", hour, minute, second);
     }
 }
 

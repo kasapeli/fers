127614e45a14e1cdf15b925079f984204278e9cb
kasapeli <yanratanaksambath@gmail.com>
2026-09-18 13:52:40 +0700

    cmos.rs: add bcd to int conversion

--- src/drivers/cmos.rs
+++ src/drivers/cmos.rs
@@ -4,10 +4,15 @@ use x86_64::instructions::port::Port;
 
 use crate::println;
 
+fn bcd_to_int(bcd: u8) -> u8 {
+    ((bcd >> 4) * 10) + (bcd & 0x0F)
+}
+
 pub fn second() {
     unsafe {
         Port::<u8>::new(0x70).write(0x00);
         let second = Port::<u8>::new(0x71).read();
+        let second = bcd_to_int(second);
         println!("{}", second);
     }
 }
@@ -16,6 +21,7 @@ pub fn minute() {
     unsafe {
         Port::<u8>::new(0x70).write(0x02);
         let minute = Port::<u8>::new(0x71).read();
+        let minute = bcd_to_int(minute);
         println!("{}", minute);
     }
 }
@@ -24,6 +30,7 @@ pub fn hour() {
     unsafe {
         Port::<u8>::new(0x70).write(0x04);
         let hour = Port::<u8>::new(0x71).read();
+        let hour = bcd_to_int(hour);
         println!("{}", hour);
     }
 }
@@ -32,12 +39,16 @@ pub fn all() {
     unsafe {
         Port::<u8>::new(0x70).write(0x00); // this is sooo messy dude
         let second = Port::<u8>::new(0x71).read();
+        let second = bcd_to_int(second);
 
         Port::<u8>::new(0x70).write(0x02);
         let minute = Port::<u8>::new(0x71).read();
+        let minute = bcd_to_int(minute);
 
         Port::<u8>::new(0x70).write(0x04);
         let hour = Port::<u8>::new(0x71).read();
+        let hour = bcd_to_int(hour);
+
         println!("{}:{}:{}", hour, minute, second);
     }
 }
